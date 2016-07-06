562a339b96990567a2653dcc535351e0f76d73c1
Jered North <jered@thenorths.net>
2016-07-06 14:55:52 -0600

    added styles to project pages and links and photos and a couple of animations

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
 
+	$('.nav-links').on('mouseover', function() {
+		$(this).css({
+			color: "green"
+		});
+	});
+
+	$('.nav-links').on('mouseout', function() {
+		$(this).css({
+			color: "white"
+		})
+	});
+
+	$('.resume-download a').on('mouseover', function() {
+		$(this).css({
+			color: "lime"
+		});
+	});
+
+	$('.resume-download a').on('mouseout', function() {
+		$(this).css({
+			color: "white"
+		})
+	});
 
 
-
-
-
-
-
-
-
-	
 });
